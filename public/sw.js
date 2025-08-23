0a39120646003c3d3ae8bc4c7dec9fdc91064c34
Alex <bateestah@gmail.com>
2025-08-23 19:39:43 -0400

    Add PWA support and iOS icon

---
+++ public/sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'road-trip-cache-v1';
+const URLS_TO_CACHE = [
+  '/',
+  '/roadtripicon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
